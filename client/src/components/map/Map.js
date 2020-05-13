a9f73bc42b52556f87d912b0fdd413c2e4875e4f
Gaith-M <mhdgaith94@gmail.com>
2020-05-14 00:00:23 +0300

    initial React setup with an outline of the main sections

---
+++ client/src/components/map/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./map.css";
+
+export const Map = () => {
+  return (
+    <section>
+      <h1>The main map will be here</h1>
+    </section>
+  );
+};
+
+export default Map;
